213b30328ff1ab786882972de31f1472e69d2902
Galyna <77695117+GalkaNH@users.noreply.github.com>
2022-10-18 13:51:02 +0200

    dodano poruszanie sie snaka

--- game.js
+++ game.js
@@ -58,3 +58,77 @@ while(aple.classList.contains('snakeBody')) {
    positionAple[1] + '"]')
 }
 aple.classList.add('aple');
+
+
+let direction = 'right';
+
+function steps() {
+   let snakeCoordinates = [snakeBody[0].getAttribute('posX'), snakeBody[0].getAttribute('posY')];
+   snakeBody[0].classList.remove('snakeHead');
+   snakeBody[snakeBody.length-1].classList.remove('snakeBody');
+   snakeBody.pop();
+   
+   
+   if (direction == 'right') {
+      //warunek przechodzenia przez sciany
+      if (snakeCoordinates[0] < 15) {
+         snakeBody.unshift(document.querySelector('[posX = "' + (+snakeCoordinates[0] +1)+ '"][posY = "' +
+         snakeCoordinates[1] + '"]'));
+      } else {
+         snakeBody.unshift(document.querySelector('[posX = "1"][posY = "' +
+         snakeCoordinates[1] + '"]'));
+      }
+   } else if (direction == 'left') {
+      if (snakeCoordinates[0] > 1) {
+         snakeBody.unshift(document.querySelector('[posX = "' + (+snakeCoordinates[0] -1)+ '"][posY = "' +
+         snakeCoordinates[1] + '"]'));
+      } else {
+         snakeBody.unshift(document.querySelector('[posX = "15"][posY = "' +
+         snakeCoordinates[1] + '"]'));
+      }
+   } else if (direction == 'up') {
+      if (snakeCoordinates[1] < 15) {
+         snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY = "' +
+         (+snakeCoordinates[1]+1) + '"]'));
+      } else {
+         snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY = "1"]'));
+      }
+   } else if (direction == 'down') {
+      if (snakeCoordinates[1] > 1) {
+         snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY = "' +
+         (+snakeCoordinates[1]-1) + '"]'));
+      } else {
+         snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY = "15"]'));
+      }
+   }
+   
+   snakeBody[0].classList.add('snakeHead');
+   for (let i = 0; i <snakeBody.length; i++) {
+      snakeBody[i].classList.add('snakeBody');
+   }
+}
+
+
+let timeSet = setInterval(steps, 300);
+
+window.addEventListener('keydown', function(e) {
+   if (e.key == "ArrowLeft" && direction!== "ArrowRight") {
+      direction = 'left';
+      console.log(true);
+   }
+
+   else if (e.key == "ArrowUp" && direction!== "ArrowDown") {
+      direction = 'up';
+      console.log(true);
+   }
+
+   else if (e.key == "ArrowRight" && direction!== "ArrowLeft") {
+      direction = 'right';
+      console.log(true);
+   }
+
+   else if (e.key == "ArrowDown" && direction!== "ArrowUp") {
+      direction = 'down';
+      console.log(true);
+   }
+});
